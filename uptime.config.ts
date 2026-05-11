f3dca107097ae6acf814ca0e392591cf946aac1d
zhaizzH <134151625+zhaizzH@users.noreply.github.com>
2026-05-11 11:26:15 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -7,7 +7,7 @@ import { MaintenanceConfig, PageConfig, WorkerConfig } from './types/config'
 
 const pageConfig: PageConfig = {
   // Title for your status page
-  title: "宅宅宅的网页状态",
+  title: "zzz's WebStatus",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
     { link: 'https://github.com/zhaizzH', label: 'GitHub' },
